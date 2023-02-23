b07923e88a0e18e424062575da8ac888e52f3d1f
priya <727721euit116@skcet.ac.in>
2023-02-23 21:42:46 +0530

    Add files via upload

---
+++ demoOne-1/src/main/java/com/example/demo/DemoOne1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOne1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOne1Application.class, args);
+	}
+
+}
